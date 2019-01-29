c3e8bcfc08385347c12df72e89d8007e4feee0c1
Odd Erik Midthaug <oesolberg@hotmail.com>
2019-01-29 19:42:41 +0100

    Downgraded and removed some comments

--- NancyWebServer/MainModule.cs
+++ NancyWebServer/MainModule.cs
@@ -11,17 +11,8 @@ namespace NancyWebServer
 			{
 				var dbStorage=new DataStorage.Repository();
 				var lastResult = dbStorage.GetLatestImageData();
-				//return new RotationData()
-				//{
-				//	Probabillity = lastResult.Probabillity,
-				//	Rotation = lastResult.Rotation,
-				//	FileChangedDate = lastResult.FileChangedDateTime,
-				//	DeliveryDate = DateTime.Now
-				//};
 				var probabillity = (lastResult.Probabillity * 100).ToString("0.00");
 				return "Probabillity=" + probabillity + ";Rotation=" + lastResult.Rotation + ";FileChangedDate=" + lastResult.FileChangedDateTime.ToString("yyyy-MM-dd HH:mm:ss") + ";DeliveryDate=" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-
-
 			};
 		}
 		
